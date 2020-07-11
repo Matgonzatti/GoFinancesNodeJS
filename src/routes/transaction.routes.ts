c39598c24e4861ac53f874b6d6e3577b9f7e65bc
Matheus Gonzatti <matheus.gonzatti@gmail.com>
2020-07-11 19:38:02 -0300

    Chore: Creating transactions features.

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,25 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+import ListTransactionService from '../services/ListTransactionService';
+import BalanceTransactionService from '../services/BalanceTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const listTransaction = new ListTransactionService(transactionsRepository);
+    const balanceTransaction = new BalanceTransactionService(
+      transactionsRepository,
+    );
+
+    const transaction = listTransaction.execute();
+    const balance = balanceTransaction.execute();
+
+    return response.json({ transactions: transaction, balance });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +27,15 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    const { title, value, type } = request.body;
+
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository,
+    );
+
+    const transaction = createTransaction.execute({ title, value, type });
+
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
